4aa264fa314a706f7619c3f510facfd2fcad346f
Vladimir <vladimir1977172mail.ru>
2023-04-03 07:51:57 +0300

    add target

--- src/test/java/vladimir/homework/products/ProductTest.java
+++ src/test/java/vladimir/homework/products/ProductTest.java
@@ -1,4 +1,28 @@
+package vladimir.homework.products;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ProductTest {
-  
+
+    Product product;
+    Product product1;
+    Product product2;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product("Nokia", 10);
+        product1 = new Product("Nokia", 10);
+        product2 = new Product("Food", 20);
+    }
+
+    @Test
+    void testEquals() {
+        boolean condition1 = product.equals(product1);
+        assertTrue(condition1);
+        boolean condition2 = product.equals(product2);
+        assertFalse(condition2);
+    }
 }
